ade0a6ac2ad72e938dac7e743d4212c5704f23a5
Juerson <zhangrson@163.com>
2024-12-05 08:21:33 +0800

    添加其它功能

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
 mod utils;
 
-use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{ get, web, App, HttpRequest, HttpResponse, HttpServer, Responder };
+use clap::{ error::ErrorKind, CommandFactory, Parser };
 use lazy_static::lazy_static;
 use local_ip_address::local_ip;
-use regex::Regex;
-use serde_json::json;
 use serde_urlencoded::from_str;
 use serde_yaml::Value as YamlValue;
-use std::fs;
+use utils::{ build, config, qrcode };
 
 const SPECIFICATION: &str = include_str!("../使用说明.txt");
 
+/// 基于HTTP传输协议的vless+ws[+tls]、trojan+ws[+tls]转换v2ray、sing-box、clash订阅工具!
+#[derive(Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// HTTP服务器的端口
+    #[arg(short, long, default_value = "10111")]
+    port: u16,
+
+    /// 默认转换为v2ray，可选singbox、clash
+    #[arg(long, default_value = "v2ray")]
+    target: String,
+}
+
+// 共享Args结构体中的数据状态（让Args在其它地方使用）
+struct AppState {
+    args: Args,
+}
+
+#[derive(Default, Clone)]
+pub struct Params {
+    pub target: String,
+    pub node_count: usize,
+    pub default_port: u16,
+    pub userid: u8,
+    pub column_name: String,
+    pub template: bool,
+    pub proxy_type: String,
+    pub tls_mode: String,
+    pub data_source: String,
+}
+
 lazy_static! {
-    static ref PROXYIES_NODE_INFO_REGEX: Regex = Regex::new(r"  - \{([^}]*(name:[^}]*)[^}]*)\}").unwrap(); // 匹配包含 "name:" 的 "- {}" 字符串
+    static ref CONFIG_FILE: &'static str = "config.yaml";
+    static ref CLASH_TEMPLATE: &'static str = "template/clash.yaml";
+    static ref SINGBOX_TEMPLATE: &'static str = "template/sing-box.json";
 }
 
 async fn default_route() -> impl Responder {
@@ -32,249 +64,154 @@ async fn index(req: HttpRequest) -> impl Responder {
     let url = format!(
         "{}://{}{}",
         req.connection_info().scheme(),
-        req.connection_info()
-            .host()
-            .replace("127.0.0.1", &ip_address),
+        req.connection_info().host().replace("127.0.0.1", &ip_address),
         req.uri()
     );
 
     // 生成二维码并将html_body嵌入网页中
-    let html_content = utils::qrcode::generate_html_with_qrcode(&html_body, &url);
+    let html_content = qrcode::generate_html_with_qrcode(&html_body, &url);
 
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(html_content)
+    HttpResponse::Ok().content_type("text/html; charset=utf-8").body(html_content)
 }
 
 #[get("/sub")]
-async fn subconverter(req: HttpRequest) -> impl Responder {
+async fn subconverter(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
     let query_str = req.query_string();
     let params: Vec<(String, String)> = from_str(&query_str).expect("Failed to parse query string");
 
-    let folder_path = "data";
-    let config_file = "config.yaml";
-    let clash_template = "template/clash.yaml";
-    let singbox_template = "template/sing-box.json";
-
-    // 参数
-    let mut target = "".to_string();
-    let mut node_count: usize = 300; // 您想要获取多少个节点？默认值是300
-    let mut port: u16 = 0; // 这里设置0，随机端口，如果设置[80,65535)，则使用这个端口，除非端口跟tls或非tls端口冲突
-    let mut tls_mode: String = "all".to_string(); // 选择哪些端口？true/1是选择TLS端口，false/0选择非TLS的端口，其它就不区分
-    let mut proxy_type = "all".to_string(); // 不区分代理的类型（vles、trojan）
-    let mut userid: u8 = 0;
-    let mut template = true; // 是否使用模板文件，默认使用
+    let mut params_control = Params {
+        target: data.args.target.to_string(), // 由cli参数中传递进来，默认转换为v2ray，可以在订阅链接中修改
+        node_count: 300, // 节点数量，这里默认300，实际不一定是这个数字
+        default_port: 0, // 默认端口，没有在数据文件读取到端口才启用它，0为随机端口
+        userid: 0, // 选择yaml中哪个节点配置（index）
+        column_name: "colo".to_string(), // 使用哪个列名的字段值为节点的前缀？可选：[colo,loc,region,city]
+        template: true, // 是否使用模板文件，默认使用
+        proxy_type: "all".to_string(), // 不区分代理的类型（vles、trojan）
+        tls_mode: "all".to_string(), // 选择哪些端口？true/1是选择TLS端口，false/0选择非TLS的端口，其它就不区分
+        data_source: "./data".to_string(), // 默认数据文件路径
+    };
 
     // 获取url的参数
     for (key, value) in params {
         if key.to_lowercase() == "target" {
-            target = value.to_string();
-        } else if key.to_lowercase() == "nodesize" || key.to_lowercase() == "nodecount" {
-            node_count = value.parse::<usize>().unwrap_or(node_count);
+            params_control.target = value.to_string().to_string();
+        } else if
+            key.to_lowercase() == "n" ||
+            key.to_lowercase() == "nodesize" ||
+            key.to_lowercase() == "nodecount"
+        {
+            params_control.node_count = value.parse::<usize>().unwrap_or(params_control.node_count);
         } else if key.to_lowercase() == "dport" || key.to_lowercase() == "defaultport" {
-            let input_port = value.parse().expect("Failed to parse setport");
-            match input_port >= 80 && input_port < 65535 {
-                true => port = input_port,
-                false => port = port,
+            if let Ok(input_port) = value.parse::<u16>() {
+                if (80..65535).contains(&input_port) {
+                    params_control.default_port = input_port;
+                }
             }
         } else if key.to_lowercase() == "tls" || key.to_lowercase() == "tlsmode" {
-            let input_tls_mode = value.to_string();
-            if ["true", "false", "1", "0"].contains(&input_tls_mode.as_str()) {
-                tls_mode = match input_tls_mode.as_str() {
-                    "1" | "true" => "true".to_string(),
-                    "0" | "false" => "false".to_string(),
-                    _ => "all".to_string(),
-                };
+            match value.to_string().to_lowercase().as_str() {
+                "1" | "true" => {
+                    params_control.tls_mode = "true".to_string();
+                }
+                "0" | "false" => {
+                    params_control.tls_mode = "false".to_string();
+                }
+                _ => {}
             }
-        } else if key.to_lowercase() == "proxy" || key.to_lowercase() == "proxytype" {
+        } else if
+            key.to_lowercase() == "type" ||
+            key.to_lowercase() == "proxy" ||
+            key.to_lowercase() == "proxytype"
+        {
             // 选择那种协议的配置节点？是vless还是trojan
-            let input_proxy_type = value.trim().to_string();
-            if ["vless", "trojan"].contains(&input_proxy_type.as_str()) {
-                proxy_type = match input_proxy_type.as_str() {
-                    "vless" => "vless".to_string(),
-                    "trojan" => "trojan".to_string(),
-                    _ => "all".to_string(),
-                };
+            match value.to_string().to_lowercase().as_str() {
+                "vless" => {
+                    params_control.proxy_type = "vless".to_string();
+                }
+                "trojan" => {
+                    params_control.proxy_type = "trojan".to_string();
+                }
+                _ => {}
             }
-        } else if key.to_lowercase() == "userid" {
-            let input_userid = value.parse::<u8>().unwrap();
-            match input_userid {
-                1..=255 => userid = input_userid,
-                _ => userid = 0,
+        } else if key.to_lowercase() == "id" || key.to_lowercase() == "userid" {
+            if let Ok(1..=255) = value.parse::<u8>() {
+                params_control.userid = value.parse::<u8>().unwrap();
             }
         } else if key.to_lowercase() == "template" {
-            template = value.parse::<bool>().unwrap_or(true);
+            params_control.template = value.parse::<bool>().unwrap_or(true);
+        } else if key.to_lowercase() == "column" || key.to_lowercase() == "columnname" {
+            // 以哪个列的字段名作为前缀？[colo,loc,region,city]
+            params_control.column_name = value.to_string();
+        } else if key.to_lowercase() == "source" || key.to_lowercase() == "datasource" {
+            params_control.data_source = value.to_string(); // 数据文件路径，支持相对路径和绝对路径
         }
     }
 
     // 限制节点数量
-    if target == "singbox" && node_count > 150 {
-        node_count = 50;
-    } else if target == "clash" && node_count > 150 {
-        node_count = 100;
+    if params_control.target == "singbox" && params_control.node_count > 150 {
+        params_control.node_count = 50;
+    } else if params_control.target == "clash" && params_control.node_count > 150 {
+        params_control.node_count = 100;
     }
 
-    /*
-    获取data文件夹中的数据
-    ip_with_none_port_vec：从本地文件中，读取到没有端口的IP、域名
-    ip_with_port_vec：从本地文件中，读取到有端口的IP（格式：IP,PORT）
-     */
-    let (ip_with_none_port_vec, ip_with_port_vec) =
-        match utils::data::read_ip_domain_from_files(folder_path, &tls_mode) {
-            Ok(result) => result,
-            Err(err) => {
-                eprintln!("Error reading IP files: {}", err);
-                return HttpResponse::InternalServerError().finish();
-            }
-        };
+    let proxies_value: YamlValue = config::get_yaml_proxies(&CONFIG_FILE);
 
-    let config_data: YamlValue = utils::config::yaml_config_to_json(&config_file);
-    let mut nodes_vec = Vec::new();
-    let mut proxy_name_vec: Vec<String> = Vec::new();
-
-    // 处理IP、域名，并生成节点信息，结果写入nodes_vec、proxy_name_vec中
-    utils::manage_ips::batch_ips(
-        ip_with_port_vec,
-        ip_with_none_port_vec,
-        &mut nodes_vec,
-        &mut proxy_name_vec,
-        config_data,
-        target.clone(),
-        port,
-        userid,
-        proxy_type,
-        tls_mode,
-        node_count,
+    // 分拣数据以及创建订阅内容
+    let html_body = build::sorting_data_and_build_subscribe(
+        proxies_value,
+        params_control.clone(),
+        &CLASH_TEMPLATE,
+        &SINGBOX_TEMPLATE
     );
 
-    // 处理输出的内容
-    if target == "clash" {
-        let mut html_body = String::new();
-        match template {
-            true => {
-                let clash_template_string = utils::file::read_file_to_string(clash_template);
-                if !nodes_vec.is_empty()
-                    && !proxy_name_vec.is_empty()
-                    && !clash_template_string.is_empty()
-                {
-                    let proxies_node_content = PROXYIES_NODE_INFO_REGEX
-                        .replace_all(&clash_template_string, &nodes_vec.join("\n"));
-                    html_body = proxies_node_content.replace(
-                        "      - 127.0.0.1:1080",
-                        &proxy_name_vec
-                            .iter_mut()
-                            .map(|name| format!("      - {}", name))
-                            .collect::<Vec<String>>()
-                            .join("\n"),
-                    );
-                }
-            }
-            false => {
-                html_body = format!("proxies:\n{}", nodes_vec.join("\n"));
-            }
-        }
-        HttpResponse::Ok()
-            .content_type("text/plain; charset=utf-8")
-            .body(html_body)
-    } else if target == "singbox" {
-        let mut html_body = String::new();
-        match template {
-            true => {
-                // sing-box配置模板
-                let template_content =
-                    fs::read_to_string(singbox_template).unwrap_or(String::new());
-                let singbox_template: serde_json::Value =
-                    serde_json::from_str(&template_content).unwrap_or(serde_json::Value::Null);
-                if !nodes_vec.is_empty()
-                    && !proxy_name_vec.is_empty()
-                    && singbox_template.is_object()
-                {
-                    let mut singbox_config = singbox_template.clone();
-                    if let Some(outbounds) = singbox_config["outbounds"].as_array_mut() {
-                        // 将节点插入到outbounds中
-                        for json_str in &nodes_vec {
-                            let parsed_json =
-                                serde_json::from_str(json_str).expect("Failed to parse JSON");
-                            outbounds.insert(2, parsed_json); // 插入到第3个位置
-                        }
-                        outbounds.iter_mut().for_each(|item| {
-                            if let Some(obj) = item.as_object_mut() {
-                                if let Some(inside_outbounds) = obj
-                                    .get_mut("outbounds")
-                                    .and_then(serde_json::Value::as_array_mut)
-                                {
-                                    // 查找并删除目标值 "{all}"、并将新值合并进来
-                                    if let Some(pos) = inside_outbounds
-                                        .iter()
-                                        .position(|x| x.as_str() == Some("{all}"))
-                                    {
-                                        // 删除"{all}"字符串
-                                        inside_outbounds.remove(pos);
-
-                                        // 将代理tag别名插入
-                                        let json_values: Vec<serde_json::Value> = proxy_name_vec
-                                            .iter()
-                                            .map(|s| serde_json::Value::String(s.clone())) // 将每个转换为 serde_json::Value
-                                            .collect();
-
-                                        // 将新数据合并到目标数组
-                                        inside_outbounds.extend(json_values);
-                                    }
-                                }
-                            }
-                        });
-                    }
-                    html_body = serde_json::to_string_pretty(&singbox_config).unwrap();
-                }
-            }
-            false => {
-                let mut outbounds = json!({"outbounds": []});
-                if let Some(array) = outbounds["outbounds"].as_array_mut() {
-                    for json_str in &nodes_vec {
-                        let parsed_json =
-                            serde_json::from_str(json_str).expect("Failed to parse JSON");
-                        array.push(parsed_json);
-                    }
-                }
-                html_body = serde_json::to_string_pretty(&outbounds).unwrap();
-            }
-        }
-        HttpResponse::Ok()
-            .content_type("text/plain; charset=utf-8")
-            .body(html_body)
-    } else {
-        let html_body = nodes_vec.join("\n");
-        HttpResponse::Ok()
-            .content_type("text/plain; charset=utf-8")
-            .body(html_body)
-    }
+    HttpResponse::Ok().content_type("text/plain; charset=utf-8").body(html_body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // 获取本机的私有IP地址
-    let local_ip = match local_ip() {
-        Ok(ip) => ip,
-        Err(e) => {
-            eprintln!("Failed to get local IP address: {}", e);
-            return Ok(());
+    // 获取命令行参数
+    let result = Args::try_parse();
+    match result {
+        Ok(args) => {
+            // 将args的cli参数值分享/传递给subconverter函数中使用
+            let shared_state = web::Data::new(AppState { args: args.clone() });
+            // 获取本机的私有IP地址
+            let local_ip = match local_ip() {
+                Ok(ip) => ip,
+                Err(e) => {
+                    eprintln!("Failed to get local IP address: {}", e);
+                    return Ok(());
+                }
+            };
+            // 绑定的端口
+            let port = args.port;
+            println!(
+                "Server is running on http://{}:{} or http://127.0.0.1:{}",
+                local_ip.to_string(),
+                port,
+                port
+            );
+            return HttpServer::new(move || {
+                App::new()
+                    .app_data(shared_state.clone())
+                    .service(index)
+                    .service(subconverter)
+                    .default_service(actix_web::web::route().to(default_route))
+            })
+                .bind(format!("0.0.0.0:{}", port))?
+                .run().await;
         }
-    };
-    // 绑定的端口
-    let port = 10111;
-    println!(
-        "Server is running on http://{}:{} or http://127.0.0.1:{}",
-        local_ip.to_string(),
-        port,
-        port
-    );
-    HttpServer::new(|| {
-        App::new()
-            .service(index)
-            .service(subconverter)
-            .default_service(actix_web::web::route().to(default_route))
-    })
-    .bind(format!("0.0.0.0:{}", port))? // 监听所有 IPv4 地址
-    .run()
-    .await
+        Err(e) => {
+            if
+                e.kind() == ErrorKind::MissingRequiredArgument ||
+                e.kind() == ErrorKind::InvalidValue
+            {
+                // 如果是因为缺少必需参数或无效值导致的错误，则显示帮助信息
+                Args::command().print_help().unwrap();
+            } else {
+                // 其他类型的错误则正常打印错误信息
+                e.print().unwrap();
+            }
+        }
+    }
+    return Ok(());
 }
