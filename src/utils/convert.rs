d054d5f9b8d2ed7270072ff6e0b65054d6dfc9d0
Juerson <zhangrson@163.com>
2025-03-28 00:29:27 +0800

    update

--- src/utils/convert.rs
+++ src/utils/convert.rs
@@ -1,4 +1,4 @@
-use crate::utils::{ clash, config, singbox, v2ray };
+use super::{ clash, config::get_yaml_value, singbox, v2ray };
 use rand::{ seq::SliceRandom, Rng };
 use serde_yaml::Value as YamlValue;
 use lazy_static::lazy_static;
@@ -9,23 +9,22 @@ lazy_static! {
 }
 
 pub fn subconvert(
-    alias_prefix: String,
-    address: String,
+    csv_alias: String,
+    csv_addr: String,
     mut port: u16,
     yamlvalue: YamlValue,
-    target: String,
-    proxy_type: String,
-    tls_mode: String,
-    userid: u8
+    uri_target: String,
+    uri_proxy_type: String,
+    uri_tls_mode: String,
+    uri_userid: u8
 ) -> (String, String) {
     // 判断端口类型的闭包
     let is_https_ports = move |port: u16| -> bool { HTTPS_PORTS.contains(&port) };
     let is_http_ports = move |port: u16| -> bool { HTTP_PORTS.contains(&port) };
 
-    let alias_prefix_new = if alias_prefix.is_empty() {
-        "".to_string()
-    } else {
-        format!("{} | ", alias_prefix)
+    let csv_remarks = match csv_alias.is_empty() {
+        true => String::new(),
+        false => format!("{} | ", csv_alias),
     };
     if let Some(sequence) = yamlvalue.clone().as_sequence_mut() {
         let length = sequence.len();
@@ -33,48 +32,59 @@ pub fn subconvert(
 
         // 循环200次，直到选中合适的节点配置为止，或循环200次才跳出循环
         for _ in 0..200 {
-            let random_https_port = HTTPS_PORTS.choose(&mut rand::thread_rng()).unwrap_or(&443);
-            let random_http_port: &u16 = HTTP_PORTS.choose(&mut rand::thread_rng()).unwrap_or(
-                &8080
-            );
-
-            index = match (1..=(length + 1) as u8).contains(&userid) {
-                true => (userid as usize) - 1, // 选择指定的账号（数组的下标）
-                false => rand::thread_rng().gen_range(0..length), // 随机选择账号（数组的下标）
+            // 使用config.yaml中具体哪个节点的配置
+            index = match (1..=(length + 1) as u8).contains(&uri_userid) {
+                true => (uri_userid as usize) - 1, // 选择指定的（数组的下标）
+                false => rand::thread_rng().gen_range(0..length), // 随机选择（数组的下标）
             };
 
-            // 获取选择的节点信息（配置文件数组的下标）
+            let random_https_port = HTTPS_PORTS.choose(&mut rand::thread_rng()).unwrap_or(&443);
+            let random_http_port = HTTP_PORTS.choose(&mut rand::thread_rng()).unwrap_or(&8080);
+
             if let Some(yaml_value) = sequence.get_mut(index) {
-                // 从配置文件中，获取到的代理类型
-                let cptype = yaml_value
+                let mut yaml_value_clone = yaml_value.clone();
+
+                let node_type = yaml_value_clone
                     .get("type")
                     .and_then(|v| v.as_str())
-                    .map(|s| s.to_lowercase())
-                    .unwrap_or_default();
+                    .map(|s| s.to_string())
+                    .unwrap_or_else(String::new);
+                let host = match node_type.as_str() {
+                    "ss" =>
+                        get_yaml_value(&yaml_value_clone, &["plugin-opts", "host"])
+                            .and_then(|v| v.as_str())
+                            .unwrap_or_default(),
+                    "vless" | "trojan" =>
+                        get_yaml_value(&yaml_value_clone, &["ws-opts", "headers", "Host"])
+                            .and_then(|v| v.as_str())
+                            .unwrap_or_default(),
+                    _ => {
+                        continue;
+                    }
+                };
 
-                let (_, host) = config::get_path_and_host_value(yaml_value);
                 let is_workers_dev = host.ends_with("workers.dev");
 
-                /*
-                 * 处理 port 、 workers.dev 的端口问题，端口不对就随机生成一个
-                 */
-                if (!is_workers_dev && port == 0) || (!is_workers_dev && is_http_ports(port)) {
+                // 处理 port 、 workers.dev 的端口问题，端口不对就随机生成一个
+                if
+                    (!is_workers_dev && port == 0) ||
+                    (!is_workers_dev && is_http_ports(port)) ||
+                    (node_type == "ss" && is_http_ports(port))
+                {
                     port = *random_https_port;
                 } else if (is_workers_dev && port == 0) || (is_workers_dev && is_https_ports(port)) {
                     port = *random_http_port;
                 }
-
-                /*
-                 * 处理 tls_mode 与 port 冲突问题
-                 */
+                // 处理 port 冲突问题（不支持所选的这个节点配置）
                 if
-                    (is_http_ports(port) && tls_mode == "true") ||
-                    (is_https_ports(port) && tls_mode == "false")
+                    (is_http_ports(port) && uri_tls_mode == "true") ||
+                    (is_https_ports(port) && uri_tls_mode == "false") ||
+                    (node_type == "ss" && is_http_ports(port))
                 {
                     continue;
                 }
 
-                if proxy_type == cptype || proxy_type == "all" {
+                if uri_proxy_type == node_type || uri_proxy_type == "all" {
                     // 节点序号/账号的序号(从1开始)
                     let padded_index = format!(
                         "{:0width$}",
@@ -85,70 +95,44 @@ pub fn subconvert(
                     let remarks: String = format!(
                         "【{}】{}{}:{}",
                         padded_index,
-                        alias_prefix_new,
-                        address,
+                        csv_remarks,
+                        csv_addr.clone(),
                         port
                     );
-
-                    match target.as_str() {
+                    match uri_target.as_str() {
                         "v2ray" => {
-                            match cptype.as_str() {
-                                "vless" => {
-                                    let vless_link = v2ray::build_vless_link(
-                                        yaml_value,
-                                        remarks.clone(),
-                                        address,
-                                        port
-                                    );
-                                    return (remarks, vless_link); // 前面是节点名称，后面是节点配置
-                                }
-                                "trojan" => {
-                                    let trojan_link = v2ray::build_trojan_linnk(
-                                        yaml_value,
-                                        remarks.clone(),
-                                        address,
-                                        port
-                                    );
-                                    return (remarks, trojan_link); // 前面是节点名称，后面是节点配置
-                                }
-                                _ => {}
+                            let (remarks_name, link) = v2ray::build_v2ray_links(
+                                &node_type,
+                                &mut yaml_value_clone,
+                                remarks,
+                                csv_addr,
+                                port
+                            );
+                            if !remarks_name.is_empty() {
+                                return (remarks_name, link);
                             }
                         }
                         "clash" => {
-                            let mut yaml_value_clone = yaml_value.clone();
-                            let clash_node = clash::build_clash_json(
+                            let clash_node = clash::build_clash_yaml(
                                 &mut yaml_value_clone,
                                 remarks.clone(),
-                                address,
+                                csv_addr,
                                 port
                             );
                             let json_node: String = serde_json::to_string(&clash_node).unwrap();
-                            let clash_with_prefix = format!("  - {json_node}");
-                            return (remarks, clash_with_prefix); // 前面是节点名称，后面是节点配置
+                            let json_string = format!("  - {json_node}");
+                            return (remarks, json_string);
                         }
                         "singbox" => {
-                            match cptype.as_str() {
-                                "vless" => {
-                                    let (remarks_name, vless_singbox) =
-                                        singbox::build_vless_singbox_config(
-                                            yaml_value,
-                                            remarks,
-                                            &address,
-                                            port
-                                        );
-                                    return (remarks_name, vless_singbox);
-                                }
-                                "trojan" => {
-                                    let (remarks_name, trojan_singbox) =
-                                        singbox::build_trojan_singbox_config(
-                                            yaml_value,
-                                            remarks,
-                                            &address,
-                                            port
-                                        );
-                                    return (remarks_name, trojan_singbox);
-                                }
-                                _ => {}
+                            let (remarks_name, json_string) = singbox::build_singbox_config_json(
+                                &node_type,
+                                &mut yaml_value_clone,
+                                remarks,
+                                csv_addr,
+                                port
+                            );
+                            if !remarks_name.is_empty() {
+                                return (remarks_name, json_string);
                             }
                         }
 
@@ -159,10 +143,8 @@ pub fn subconvert(
                 }
             }
         }
-    } else {
-        println!("不是序列，config.yaml文件的书写格式有问题，请检查！");
     }
 
     // 返回的前面是节点名称，后面是节点配置
-    return ("".to_string(), "".to_string());
+    return (String::new(), String::new());
 }
