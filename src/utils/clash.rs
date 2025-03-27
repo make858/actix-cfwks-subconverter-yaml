2a94f5b79fb31b10a746bcff8bd7af1a37499e84
Juerson <zhangrson@163.com>
2025-03-28 00:28:03 +0800

    简化代码

--- src/utils/clash.rs
+++ src/utils/clash.rs
@@ -1,42 +1,22 @@
-use serde_yaml::Value;
+use serde_yaml::Value as YamlValue;
 
-/// 原来就是clash配置的，直接修改对应的值即可
-#[allow(dead_code)]
-pub fn build_clash_json(
-    yaml_value: &mut Value,
-    set_remarks: String,
-    set_server: String,
-    set_port: u16
-) -> &mut Value {
-    if let Some(name) = yaml_value.get_mut(&Value::String("name".to_string())) {
-        match name {
-            Value::String(ref mut name_str) => {
-                *name_str = set_remarks.into();
-            }
-            _ => unreachable!("name字段不是字符串类型"),
+// 原来就是clash配置的，直接修改对应的值即可
+pub fn build_clash_yaml(
+    yaml_value: &mut YamlValue,
+    remarks: String,
+    server_address: String,
+    server_port: u16
+) -> &mut YamlValue {
+    if let YamlValue::Mapping(map) = yaml_value {
+        if let Some(name_value) = map.get_mut("name") {
+            *name_value = YamlValue::String(remarks);
         }
-    } else {
-        unreachable!("name字段不存在");
-    }
-    if let Some(server) = yaml_value.get_mut(&Value::String("server".to_string())) {
-        match server {
-            Value::String(ref mut server_str) => {
-                *server_str = set_server.into();
-            }
-            _ => unreachable!("server字段不是字符串类型"),
+        if let Some(server_value) = map.get_mut("server") {
+            *server_value = YamlValue::String(server_address);
         }
-    } else {
-        unreachable!("server字段不存在");
-    }
-    if let Some(port) = yaml_value.get_mut(&Value::String("port".to_string())) {
-        match port {
-            Value::Number(ref mut port_num) => {
-                *port_num = set_port.into();
-            }
-            _ => unreachable!("port字段不是数字类型"),
+        if let Some(port_value) = map.get_mut("port") {
+            *port_value = YamlValue::Number(server_port.into());
         }
-    } else {
-        unreachable!("port字段不存在");
     }
     yaml_value
 }
