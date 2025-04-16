77a580b4cb016dbb3040eb984e7cced05a2a6981
Juerson <zhangrson@163.com>
2025-04-17 00:00:47 +0800

    处理端口问题

--- src/utils/convert.rs
+++ src/utils/convert.rs
@@ -1,12 +1,6 @@
-use super::{ clash, config::get_yaml_value, singbox, v2ray };
-use rand::{ seq::SliceRandom, Rng };
+use super::{clash, config::get_yaml_value, singbox, v2ray};
+use rand::{seq::SliceRandom, Rng};
 use serde_yaml::Value as YamlValue;
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref HTTP_PORTS: [u16; 7] = [80, 8080, 8880, 2052, 2082, 2086, 2095];
-    static ref HTTPS_PORTS: [u16; 6] = [443, 2053, 2083, 2087, 2096, 8443];
-}
 
 pub fn subconvert(
     csv_alias: String,
@@ -16,11 +10,13 @@ pub fn subconvert(
     uri_target: String,
     uri_proxy_type: String,
     uri_tls_mode: String,
-    uri_userid: u8
+    uri_userid: u8,
+    http_ports: &[u16; 7],
+    https_ports: &[u16; 6],
 ) -> (String, String) {
     // 判断端口类型的闭包
-    let is_https_ports = move |port: u16| -> bool { HTTPS_PORTS.contains(&port) };
-    let is_http_ports = move |port: u16| -> bool { HTTP_PORTS.contains(&port) };
+    let is_https_ports = move |port: u16| -> bool { https_ports.contains(&port) };
+    let is_http_ports = move |port: u16| -> bool { http_ports.contains(&port) };
 
     let csv_remarks = match csv_alias.is_empty() {
         true => String::new(),
@@ -38,8 +34,8 @@ pub fn subconvert(
                 false => rand::thread_rng().gen_range(0..length), // 随机选择（数组的下标）
             };
 
-            let random_https_port = HTTPS_PORTS.choose(&mut rand::thread_rng()).unwrap_or(&443);
-            let random_http_port = HTTP_PORTS.choose(&mut rand::thread_rng()).unwrap_or(&8080);
+            let random_https_port = https_ports.choose(&mut rand::thread_rng()).unwrap_or(&443);
+            let random_http_port = http_ports.choose(&mut rand::thread_rng()).unwrap_or(&8080);
 
             if let Some(yaml_value) = sequence.get_mut(index) {
                 let mut yaml_value_clone = yaml_value.clone();
@@ -49,48 +45,51 @@ pub fn subconvert(
                     .and_then(|v| v.as_str())
                     .map(|s| s.to_string())
                     .unwrap_or_else(String::new);
+
                 let host = match node_type.as_str() {
-                    "ss" =>
-                        get_yaml_value(&yaml_value_clone, &["plugin-opts", "host"])
-                            .and_then(|v| v.as_str())
-                            .unwrap_or_default(),
-                    "vless" | "trojan" =>
+                    "ss" => get_yaml_value(&yaml_value_clone, &["plugin-opts", "host"])
+                        .and_then(|v| v.as_str())
+                        .unwrap_or_default(),
+                    "vless" | "trojan" => {
                         get_yaml_value(&yaml_value_clone, &["ws-opts", "headers", "Host"])
                             .and_then(|v| v.as_str())
-                            .unwrap_or_default(),
+                            .unwrap_or_default()
+                    }
                     _ => {
                         continue;
                     }
                 };
 
+                // 处理 port 、 workers.dev、tls 之间与端口的问题，端口不对就随机生成一个
                 let is_workers_dev = host.ends_with("workers.dev");
-
-                // 处理 port 、 workers.dev 的端口问题，端口不对就随机生成一个
-                if
-                    (!is_workers_dev && port == 0) ||
-                    (!is_workers_dev && is_http_ports(port)) ||
-                    (node_type == "ss" && is_http_ports(port))
-                {
-                    port = *random_https_port;
-                } else if (is_workers_dev && port == 0) || (is_workers_dev && is_https_ports(port)) {
-                    port = *random_http_port;
+                if node_type == "ss" {
+                    let tls_val = get_yaml_value(&yaml_value_clone, &["plugin-opts", "tls"])
+                        .and_then(|v| v.as_bool())
+                        .unwrap_or(true);
+                    if tls_val && (port == 0 || is_http_ports(port)) {
+                        port = *random_https_port;
+                    } else if !tls_val && (port == 0 || is_https_ports(port)) {
+                        port = *random_http_port;
+                    }
+                } else if ["vless", "trojan"].contains(&node_type.as_str()) {
+                    if is_workers_dev && (port == 0 || is_https_ports(port)) {
+                        port = *random_http_port;
+                    } else if !is_workers_dev && (port == 0 || is_http_ports(port)) {
+                        port = *random_https_port;
+                    }
                 }
-                // 处理 port 冲突问题（不支持所选的这个节点配置）
-                if
-                    (is_http_ports(port) && uri_tls_mode == "true") ||
-                    (is_https_ports(port) && uri_tls_mode == "false") ||
-                    (node_type == "ss" && is_http_ports(port))
+
+                // 根据uri的tls筛选节点配置，是否使用这个节点生成订阅
+                if (is_http_ports(port) && uri_tls_mode == "true")
+                    || (is_https_ports(port) && uri_tls_mode == "false")
                 {
                     continue;
                 }
 
                 if uri_proxy_type == node_type || uri_proxy_type == "all" {
                     // 节点序号/账号的序号(从1开始)
-                    let padded_index = format!(
-                        "{:0width$}",
-                        index + 1,
-                        width = length.to_string().len()
-                    );
+                    let padded_index =
+                        format!("{:0width$}", index + 1, width = length.to_string().len());
                     // 构建完整的节点名称
                     let remarks: String = format!(
                         "【{}】{}{}:{}",
@@ -106,7 +105,7 @@ pub fn subconvert(
                                 &mut yaml_value_clone,
                                 remarks,
                                 csv_addr,
-                                port
+                                port,
                             );
                             if !remarks_name.is_empty() {
                                 return (remarks_name, link);
@@ -117,7 +116,7 @@ pub fn subconvert(
                                 &mut yaml_value_clone,
                                 remarks.clone(),
                                 csv_addr,
-                                port
+                                port,
                             );
                             let json_node: String = serde_json::to_string(&clash_node).unwrap();
                             let json_string = format!("  - {json_node}");
@@ -129,7 +128,7 @@ pub fn subconvert(
                                 &mut yaml_value_clone,
                                 remarks,
                                 csv_addr,
-                                port
+                                port,
                             );
                             if !remarks_name.is_empty() {
                                 return (remarks_name, json_string);
